c301b312dbf8d068b80ac59b9fa3c0f85d874a03
CallbackToTheRescue <59921779+CallbackToTheRescue@users.noreply.github.com>
2020-04-02 21:10:08 +0200

    Update defer.js

--- defer.js
+++ defer.js
@@ -1,1 +1,12 @@
+Function.prototype.defer = function (ms) {
+  let myProto = this
+  return function (args) {
+  setTimeout(() => myProto.apply(this, [args]), ms)
+  }
+}
 
+function myProto(name) {
+  console.log(`Hello ${name}`)
+}
+
+myProto.defer(5000)('Edoardo')
